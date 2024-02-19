fd379f6f59bccc86ce01d0b40a643aedd153da3e
Joeysoda <zj368835@gmail.com>
2024-02-19 22:24:54 +0800

    first

---
+++ 20240219/src/FORDEMO.java
@@ -0,0 +1,9 @@
+public class FORDEMO {
+public static void main(String[] args){
+    double count;
+    for(count = 0; count < 5; count = count + 1){
+        System.out.println("This is count: "+ count);
+    }
+    System.out.println("Done!");
+}
+}
---
+++ 20240219/src/abc.java
@@ -0,0 +1,10 @@
+public class abc {
+    public static void main(String[] args){
+        int var1;
+        int var2;
+        var1 = 1024;
+        System.out.println("var1 contains "+var1);
+        var2= var1/3;
+        System.out.println("var2 contains var1/3: "+ var2);
+    }
+}
