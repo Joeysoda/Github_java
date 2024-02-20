29bff92ba173cdc90a6ef2255f4e88b4fe118ebf
Joeysoda <zj368835@gmail.com>
2024-02-20 21:41:11 +0800

    net

--- 20240220/src/helloworld.java
+++ 20240220/src/helloworld.java
@@ -8,7 +8,19 @@ public class helloworld {
      * 这个是一个main方法
      * @param args 这是一个数组
      */
+    //数据类型与变量
     public static void main(String[] args) {
-        System.out.println("你好！");
+        /*
+        大小是四个字节，不分是多少位的操作系统
+         */
+        int a = 10;
+        //输出int最大值和最小值
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        int c =2147483647;
+        System.out.println(c);//2147483647
+        int d = c+1;
+        System.out.println(d);//-2147483648 overflow
     }
 }
