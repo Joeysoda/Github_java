66969bc76db529145fe45c6720f83e539adeb09f
Joeysoda <zj368835@gmail.com>
2024-02-20 21:26:06 +0800

    first ppt

---
+++ 20240220/src/helloworld.java
@@ -0,0 +1,14 @@
+/**
+ * @author JieZHANG
+ * @version 1.0.0
+ *作用helloworld类，入门第一个程序练习
+ */
+public class helloworld {
+    /**
+     * 这个是一个main方法
+     * @param args 这是一个数组
+     */
+    public static void main(String[] args) {
+        System.out.println("你好！");
+    }
+}
---
+++ 20240220/src/myHello/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"helloworld","l":"helloworld()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"helloworld","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
