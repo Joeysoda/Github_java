0041db2b9a90373953993bd01dd13484d7e9ae8f
Joeysoda <zj368835@gmail.com>
2024-02-25 20:28:56 +0800

    not finished

---
+++ 20240225/src/simulated_landing.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+/**
+ * Description:
+ * Date: 2024-02-25
+ * Time: 20:07
+ */
+public class simulated_landing {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int count = 3;
+        int secret=123456;
+        System.out.println("请输入一个密码");
+        String password = in.nextLine();
+        if(password.equals(secret))
+    }
+
+}
