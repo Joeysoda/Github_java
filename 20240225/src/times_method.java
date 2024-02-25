672b952d4ac741e982859565e8cc8b203762c6ea
Joeysoda <zj368835@gmail.com>
2024-02-25 15:09:27 +0800

    times_method

---
+++ 20240225/src/times_method.java
@@ -0,0 +1,17 @@
+/**
+ * Description:
+ * Date: 2024-02-25
+ * Time: 14:59
+ */
+public class times_method {
+    public static void main(String[] args) {
+        for(int i=1;i<=9;i++){
+            for(int j=1;j<=9;j++){
+                if(i>=j){
+                System.out.print(j+"*"+i+"="+j*i+"       ");
+            }}
+            System.out.println(" ");
+        }
+    }
+
+}
