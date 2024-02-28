fb5de4b0502843c0634c83407bf7536d7cd6a0f8
Joeysoda <zj368835@gmail.com>
2024-02-28 22:24:00 +0800

    array

---
+++ 20240228/src/array.java
@@ -0,0 +1,14 @@
+/**
+ * Description:
+ * Date: 2024-02-28
+ * Time: 21:45
+ */
+public class array {
+    public static void main(String[] args) {
+        int [] array={1,2,3,4,5};
+        int [] array2={5};
+       for (int i=0;i<4;i++){
+           System.out.println(array[i]);
+       }
+    }
+}
