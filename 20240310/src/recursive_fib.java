018ea24d54a5c2530a3fe3e95a60ef86833d394a
Joeysoda <zj368835@gmail.com>
2024-03-11 00:11:38 +0800

    recursive_fib

--- 20240310/src/recursive_fib.java
+++ 20240310/src/recursive_fib.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 /**
  * Description:
  * Date: 2024-03-10
- * Time: 23:47
+ * Time: 23:58
  */
-public class recursive_sum_of_digit {
+public class recursive_fib {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int num1 = in.nextInt();
-        int n=sum(num1);
+        int n=fib(num1);
         System.out.println(n);
     }
-        public static int sum(int n){
-            if(n<10){
-                return n;
-            }
-
-                return n%10+sum(n/10);
-
-
+    public static int fib(int num1){
+        if(num1==1||num1==2){
+            return 1;
         }
+        else{
+            return fib(num1-1)+fib(num1-2);
+        }
+    }
 
 }
