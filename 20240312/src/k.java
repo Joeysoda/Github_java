2052bb65659fe5da5f4a5e489daf4beaf49fbbde
Joeysoda <zj368835@gmail.com>
2024-03-12 22:25:21 +0800

    实例化

---
+++ 20240312/src/k.java
@@ -0,0 +1,52 @@
+/**
+ * Description:
+ * Date: 2024-03-12
+ * Time: 22:20
+ */
+public class k {/**
+ * Description:
+ * Date: 2024-03-12
+ * Time: 22:13
+ */
+
+    /*
+        属性/成员变量
+        1.定义在类的内部，方法的外部
+        2.默认值是null
+     */
+    public String name;
+    public int age;
+    public String sex;
+
+    //成员方法
+    public void eat(){
+        System.out.println(name+" 正在吃饭");
+    }
+    public void sleep(){
+        System.out.println(name+"正在睡觉");
+    }
+    public void show(){
+        System.out.println("姓名: "+name+"年龄："+age+"性别："+sex);
+
+    }
+
+    public class Test{
+        public static void main(String[] args) {
+            //类的实例化
+            Person person1=new Person();
+            person1.age = 10;
+            person1.name = "zhangsan";
+            person1.sex = "男";
+            person1.show();
+
+            Person person2=new Person();
+            person2.age = 10;
+            person2.name = "zhangsan";
+            person2.sex = "男";
+            person2.show();
+        }
+    }
+}
+
+
+
