925470ecf5002736172f5c303deea3e5a8f66cdf
Joeysoda <zj368835@gmail.com>
2024-06-14 15:18:48 +0800

    总结: 向上转型的三个方法

--- 继承多态2/src/demo1/Test1.java
+++ 继承多态2/src/demo1/Test1.java
@@ -4,89 +4,74 @@ package demo1;
  * Description:
  * Date: 2024-06-13
  * Time: 16:42
- */
+ *
+ *
+*/
 class Animal {
     public String name;
     public int age;
 
-    public void eat() {
-        System.out.println(this.name + " eating");
-    }
 
     public Animal(String name, int age) {
         this.name = name;
         this.age = age;
     }
+    public void eat(){
+        System.out.println(this.name+"eating");
+    }
 }
 
-class Dog extends Animal {
+class Dog extends Animal{
 
-    public Dog() {
-        super("不带参数构造", 1);
-    }
 
     public Dog(String name, int age) {
         super(name, age);
     }
 
-    public void bark() {
-        System.out.println(this.name + " Barking");
-    }
-
     @Override
-    public void eat() {
-        System.out.println(this.name + " Eating dog food!");
+    public void eat(){
+        System.out.println(this.name+" Eating Dog food");
     }
 }
 
-class Bird extends Animal {
+class Bird extends Animal{
     public Bird(String name, int age) {
         super(name, age);
     }
-
-    public void gigi() {
-        System.out.println(this.name + " jijiing");
-    }
-
     @Override
-    public void eat() {
-        super.eat();
-        System.out.println(this.name + " 正在吃鸟粮");
+    public void eat(){
+        System.out.println(this.name+" Eating Bird food");
     }
 }
 
-public class Test1 {
-    // 向上转型方法1: 方法传参
-    public static void func(Animal animal1) {
-        animal1.eat();
-        System.out.println("向上转型1");
+public class Test1{
+
+    public static void func(Animal animal2){
+        animal2.eat();
+        System.out.println("方法传参结束\n");
 
     }
 
-    // 向上转型方法2: 方法返回, 返回任意子类对象
-    public static Animal func2() {
-        return new Dog("向上转型2", 10);
+    public static Animal func2(){
+        return new Dog("dog3",3);
     }
+
 
     public static void main(String[] args) {
-        // 向上转型方法1: 方法传参
-        Dog dog = new Dog();
-        func(dog); // Dog 对象作为 Animal 参数传递
 
-        // 向上转型方法3: 直接赋值
-        Animal animal2 = new Dog("向上转型3", 5);
+        //直接赋值
+        System.out.println("直接赋值开始");
+        Animal animal1 = new Dog("dog1",1);
+        animal1.eat();
+        System.out.println("直接赋值结束\n");
 
-        // 向上转型方法2: 方法返回
-        Animal animal3 = func2();
+        System.out.println("方法传参开始");
+        func( new Dog("dog2",1));
+        //goto line 49
 
-        // 调用父类方法
-        animal2.eat(); // 调用 Dog 类的 eat 方法，因为 Dog 重写了 eat 方法
-        animal3.eat(); // 调用 Dog 类的 eat 方法，因为 func2() 返回 Dog 对象
-
-        // 调用子类特有的方法，需要向下转型
-        if (animal2 instanceof Dog) {
-            Dog dog2 = (Dog) animal2; // 向下转型
-            dog2.bark();
-        }
+        System.out.println("返回值法开始");
+        Animal animal3=func2();
+        animal3.eat();
+        System.out.println("返回值法结束\n");
     }
 }
