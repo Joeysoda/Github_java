0cada1725eef1fe0e5d5a602078bae3f8e4ea088
Joeysoda <zj368835@gmail.com>
2024-07-07 16:22:15 +0800

    在使用switch时,用yield 和->替换

---
+++ 20240707_补充语法/src/Test.java
@@ -0,0 +1,30 @@
+import com.sun.tools.javac.Main;
+
+/**
+ * Description:
+ * Date: 2024-07-07
+ * Time: 16:11
+ */
+public class Test {
+    public static void main(String[] args) {
+        String data = "one";
+        int result = switch (data){
+            case "one": yield 1;
+            case "two": yield 2;
+            case "three" : yield 3;
+            default : yield 4;
+        };
+        System.out.println(result);
+    }
+
+    public static void main1(String[] args) {
+        String data = "one";
+        int result = switch (data){
+            case "one"-> 1;
+            case "two"-> 2;
+            case "three" ->3;
+            default->4;
+        };
+        System.out.println(result);
+    }
+}
