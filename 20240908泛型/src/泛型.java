3cd2ee271c5278215f6fc9917b847d8593e20d3c
Joeysoda <zj368835@gmail.com>
2024-09-09 21:47:34 +0800

    泛型

--- 20240908泛型/src/泛型.java
+++ 20240908泛型/src/泛型.java
@@ -5,8 +5,8 @@
  */
 
 class MyArray<T>{
-    //public Object[] array = new Object[10];
-    public T[] array = (T[])new Object[10];
+    public Object[] array = new Object[10];
+    //public T[] array = (T[])new Object[10];
 
     public void setValue(int pos, T x){
         array[pos]=x;
@@ -21,13 +21,14 @@ class MyArray<T>{
 public class 泛型 {
     public static void main(String[] args) {
 
+
         MyArray<Integer> myArray = new MyArray<>();
         MyArray<String> myArray2 = new MyArray<>();
         myArray.setValue(0,10);
         myArray2.setValue(1,"element1");
 
         Integer x = myArray.getValue(0);
-        Integer s = myArray.getValue(0);
+        String s = myArray2.getValue(1);
 
         System.out.println(x);
         System.out.println(s);
