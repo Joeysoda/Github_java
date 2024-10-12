bed5e2fc8ccbfe48e978de892e921bf4a94fa01a
Joeysoda <zj368835@gmail.com>
2024-10-12 23:59:09 +0800

    good

--- 20240220/out/production/20240220/myHello/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"helloworld","l":"helloworld()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"helloworld","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
