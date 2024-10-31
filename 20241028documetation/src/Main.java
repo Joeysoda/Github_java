c9b4347cbcec283331f96362de0d2cfe37193287
Joeysoda <zj368835@gmail.com>
2024-10-31 23:33:20 +0800

    good

---
+++ 20241028documetation/src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Description:
+ * Date: 2024-10-28
+ * Time: 9:25
+ */
+public class Main {
+    /**
+     * Documentation
+     * @param args Arguement List for the main method
+     *
+     *
+     *
+     *
+     *
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
