4bef1654bbb51905304386547aa7a7bc8c0355c8
Joeysoda <zj368835@gmail.com>
2024-10-31 23:34:09 +0800

    good

---
+++ 20241028documetation/src/People.java
@@ -0,0 +1,20 @@
+/**
+ * Description:
+ * Date: 2024-10-28
+ * Time: 9:29
+ */
+public class People {
+
+    String name;
+    public People(){
+
+    }
+    /**
+     * This the the getter of the name
+     * @return Value of the name
+     */
+
+    public String getName() {
+        return name;
+    }
+}
