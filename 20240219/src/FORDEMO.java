d7456a290e99d020b5bf7ea84f1b6250e0447a5e
Joeysoda <zj368835@gmail.com>
2024-12-19 23:59:42 +0800

    Addd

--- 20240219/src/FORDEMO.java
+++
@@ -1,9 +0,0 @@
-public class FORDEMO {
-public static void main(String[] args){
-    double count;
-    for(count = 0; count < 5; count = count + 1){
-        System.out.println("This is count: "+ count);
-    }
-    System.out.println("Done!");
-}
-}
