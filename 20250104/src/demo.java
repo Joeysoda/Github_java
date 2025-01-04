0873800f2647ad5465c7cff227f8d78d6b2fc005
Joeysoda <zj368835@gmail.com>
2025-01-04 22:54:25 +0800

    展示Aggregatioin和Composittion

--- 20250104/src/demo.java
+++ 20250104/src/demo.java
@@ -1,7 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Description:
+ * Description:展示Aggregatioin和Composittion
  * Date: 2025-01-04
  * Time: 22:30
  */
 public class demo {
+
+    class Team {
+        private List<Employee> employees;
+
+        public Team() {
+            employees = new ArrayList<>();
+        }
+
+        public void addEmployee(Employee emp) {
+            employees.add(emp);
+        }
+    }
+
+    class Employee {
+        private String name;
+
+        public Employee(String name) {
+            this.name = name;
+        }
+    }
+
+
+
+
+
+
+    public class House{
+
+        public House(){
+        }
+
+        private class Brick{
+
+            public Brick (){
+
+            }
+        }
+    }
 }
