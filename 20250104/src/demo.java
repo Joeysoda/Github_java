3d19c7e3d602a8b146b9f97d83b585eda2a9387f
Joeysoda <zj368835@gmail.com>
2025-01-05 20:34:54 +0800

    展示修改违反Open Close Principle 的代码,两个例子

--- 20250104/src/demo.java
+++ 20250104/src/demo.java
@@ -71,7 +71,7 @@ public class demo {
 //        public void sleep(){}
 }
     interface  Running{
-        void run();
+        public abstract void run();
     }
 
 //    class Dog extends Animal implements Running{
@@ -86,6 +86,113 @@ public class demo {
 //        public void run(){
 //
 //        }
+
+
+
+
+
+
+
+
+    //=================违反Open Close Principle===================
+    //=======================例子1=========================
+//    public double calculateSalary(Employee employee) {
+//        double salary = 0.0;
+//        if (employee is Developer) {
+//            Developer developer = (Developer) employee;
+//            salary += employee.workingHours * employee.hourlyRate;
+//        }
+//  else if (employee is TeamLeader) {
+//            TeamLeader teamLeader = (TeamLeader) employee;
+//            salary += teamLeader.workingHours * teamLeader.hourlyRate * teamLeader.bonus;
+//        }
+//  else if (employee is Recruiter) {
+//            Recruiter recruiter = (Recruiter) recruiter;
+//            salary += recruiter.workingHours * recruiter.hourlyRate * recruiter.bonus * 1.5;
+//        }
+//
+//        return salary;
+//    }
+
+    public abstract  class Employee{
+        public abstract  double calculateSalary();
+        public double hours, rates, bonus;
+
+    }
+    public class Developer extends  Employee{
+
+        @Override
+        public double calculateSalary(){
+            return hours * rates;
+        }
+    }
+    public class TeamLeader extends  Employee{
+
+        @Override
+        public double calculateSalary(){
+            return hours * rates * bonus;
+        }
+    }
+    public class Recruiter extends  Employee{
+
+        @Override
+        public double calculateSalary(){
+            return hours * rates * bonus;
+        }
+    }
+
+//========================例题2===============================
+//public class NotificationService {
+//
+//    public void sendNotification(String notificationType, String message) {
+//        if (notificationType.equalsIgnoreCase("EMAIL")) {
+//            sendEmail(message);
+//        } else if (notificationType.equalsIgnoreCase("SMS")) {
+//            sendSMS(message);
+//        } else if (notificationType.equalsIgnoreCase("PUSH")) {
+//            sendPush(message);
+//        }
+//    }
+//
+//    private void sendEmail(String message) {
+//        System.out.println("Sending EMAIL notification: " + message);
+//    }
+//
+//    private void sendSMS(String message) {
+//        System.out.println("Sending SMS notification: " + message);
+//    }
+//
+//    private void sendPush(String message) {
+//        System.out.println("Sending PUSH notification: " + message);
+//    }
+//}
+    public abstract class NotificationService{
+        public  String message;
+        public abstract String sendNotification ();
+}
+    private class EMAIL extends NotificationService{
+
+        @Override
+        public String sendNotification(){
+        return "Sending EMAIL notification: "+message;
+        }
+    }
+    private class SMS extends NotificationService{
+
+        @Override
+        public String sendNotification(){
+            return "Sending EMAIL notification: "+message;
+        }
+    }
+    private class PUSH extends NotificationService{
+
+        @Override
+        public String sendNotification(){
+            return "Sending EMAIL notification: "+message;
+        }
+    }
+
+
 //    }
 
 
