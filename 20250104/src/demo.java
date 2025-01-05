61b33e583b80d15a657202329889e36f383a5843
Joeysoda <zj368835@gmail.com>
2025-01-05 15:34:43 +0800

    展示Override 和overload

--- 20250104/src/demo.java
+++ 20250104/src/demo.java
@@ -8,30 +8,99 @@ import java.util.List;
  */
 public class demo {
 
-class Team {
-    public Team (Person person){
+//class Team {
+//    public Team (Person person){
+//
+//    }
+//}
+//class Person{
+//
+//    public Person(){
+//
+//    }
+//}//low coupling
+//
+//
+//
+//
+//
+//class House {
+//    public House(Brick brick){
+//
+//    }
+//    class Brick {
+//        public Brick(){
+//
+//        }
+//    }
+//  }
+//    //解释overload override
+//    class Calculator{
+//        public int add(int a, int b){
+//            return a + b;
+//        }
+//
+//        public double add (double a, double b){
+//            return a +b;
+//        }
+//    }
+//
+//
+//    class Animal{
+//        public void sound(){
+//            System.out.println("Animal sound");
+//        }
+//    }
+//
+//    class Dog extends Animal{
+//        @Override
+//        public void sound(){
+//            System.out.println("Dog sound");
+//        }
+//    }
+//
+abstract class Animal {
+    protected String name;
 
+    public Animal(String name) {
+        this.name = name;
     }
+
+    public void sleep() {
+        System.out.println(name + " 正在睡觉");
+    }
+
+    public abstract void makeSound();
 }
-class Person{
 
-    public Person(){
+    class Dog extends Animal {
+        public Dog(String name) {
+            super(name);
+        }
 
-    }
-}//low coupling
-
-
-
-
-
-class House {
-    public House(Brick brick){
-
-    }
-    class Brick {
-        public Brick(){
-
+        @Override
+        public void makeSound() {
+            System.out.println(name + " 在汪汪叫");
         }
     }
-  }
+
+    interface Flyable {
+        void fly();
+    }
+
+    interface Swimmable {
+        void swim();
+    }
+
+    class Duck implements Flyable, Swimmable {
+        @Override
+        public void fly() {
+            System.out.println("鸭子在飞");
+        }
+
+        @Override
+        public void swim() {
+            System.out.println("鸭子在游泳");
+        }
+    }
 }
