af41bdd5d1aa520ea50c39a8036d4c968af20bdf
Joeysoda <zj368835@gmail.com>
2025-01-05 15:54:27 +0800

    展示abstract interface

--- 20250104/src/demo.java
+++ 20250104/src/demo.java
@@ -1,3 +1,4 @@
+import javax.xml.namespace.QName;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -59,48 +60,105 @@ public class demo {
 //        }
 //    }
 //
-abstract class Animal {
-    protected String name;
+    abstract class Animal{
 
-    public Animal(String name) {
-        this.name = name;
-    }
-
-    public void sleep() {
-        System.out.println(name + " 正在睡觉");
-    }
-
-    public abstract void makeSound();
+        public abstract void swim();
+//        public String name;
+//        public Animal(String name){
+//            this.name=name;
+//        }
+//
+//        public void sleep(){}
 }
-
-    class Dog extends Animal {
-        public Dog(String name) {
-            super(name);
-        }
-
-        @Override
-        public void makeSound() {
-            System.out.println(name + " 在汪汪叫");
-        }
+    interface  Running{
+        void run();
     }
 
-    interface Flyable {
-        void fly();
-    }
+//    class Dog extends Animal implements Running{
+//        public Dog(){
+//            super("name");
+//        }
+//        @Override
+//        public void swim(){
+//            System.out.println("a");
+//        }
+//        @Override
+//        public void run(){
+//
+//        }
+//    }
 
-    interface Swimmable {
-        void swim();
-    }
 
-    class Duck implements Flyable, Swimmable {
-        @Override
-        public void fly() {
-            System.out.println("鸭子在飞");
-        }
 
-        @Override
-        public void swim() {
-            System.out.println("鸭子在游泳");
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//abstract class Animal {
+//    protected String name;
+//
+//    public Animal(String name) {
+//        this.name = name;
+//    }
+//
+//    public void sleep() {
+//        System.out.println(name + " 正在睡觉");
+//    }
+//
+//    public abstract void makeSound();
+//}
+//
+//    class Dog extends Animal {
+//        public Dog(String name) {
+//            super(name);
+//        }
+//
+//        @Override
+//        public void makeSound() {
+//            System.out.println(name + " 在汪汪叫");
+//        }
+//    }
+//
+//    interface Flyable {
+//        void fly();
+//    }
+//
+//    interface Swimmable {
+//        void swim();
+//    }
+//
+//    class Duck extends Animal implements Flyable, Swimmable {
+//        public Duck(String name) {
+//            super(name);
+//        }
+//
+//        @Override
+//        public void fly() {
+//            System.out.println("鸭子在飞");
+//        }
+//
+//        @Override
+//        public void swim() {
+//            System.out.println("鸭子在游泳");
+//        }
+//
+//        @Override
+//        public void makeSound() {
+//
+//        }
+//    }
 }
