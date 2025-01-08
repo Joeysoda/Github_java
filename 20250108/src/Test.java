1bc0cba6e97db97504ade0410da3f42e93007237
Joeysoda <zj368835@gmail.com>
2025-01-08 12:31:34 +0800

    z

---
+++ 20250108/src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Description:
+ * Date: 2025-01-08
+ * Time: 11:36
+ */
+interface Animal{
+    void eat();
+}
+abstract class Animal2{
+    abstract void eat();
+}
+
+public class Test {
+}
